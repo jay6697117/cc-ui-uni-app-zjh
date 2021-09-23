6985c48c64e0f70abd29b5c3b3bfcc781cb81151
steve zhang <871506263@qq.com>
2021-09-23 14:42:59 +0800

    提交

--- components/cc-dialog/cc-dialog的副本.vue
+++ components/cc-dialog/cc-dialog的副本.vue
@@ -1,29 +1,11 @@
 <template>
   <view>
-    <view
-      :class="{ 'cc-dialog-mask-show': show, 'cc-dialog-mask-hide': !show }"
-      class="cc-dialog-mask"
-      :style="{ top, display }"
-      @click="clickMask"
-    ></view>
-    <view
-      :class="{ 'cc-dialog-show': show, 'cc-dialog-hide': !show }"
-      class="cc-dialog"
-      :style="{ width: width + 'rpx', display }"
-    >
+    <view :class="{ 'cc-dialog-mask-show': show, 'cc-dialog-mask-hide': !show }" 
+     class="cc-dialog-mask" :style="{ top, display }" @click="clickMask"></view>
+    <view :class="{ 'cc-dialog-show': show, 'cc-dialog-hide': !show }" class="cc-dialog" :style="{ width: width + 'rpx', display }">
       <view class="cc-dialog-content">
-        <view
-          :class="{ 'cc-dialog-content-nocontent': !content && !$slots.content }"
-          class="cc-dialog-content-title"
-          v-if="title"
-        >
-          {{ title }}
-        </view>
-        <view
-          :class="{ 'cc-dialog-content-content-padding': !$slots.content }"
-          class="cc-dialog-content-content"
-          v-if="content || $slots.content"
-        >
+        <view :class="{ 'cc-dialog-content-nocontent': !content && !$slots.content }" class="cc-dialog-content-title" v-if="title">{{ title }}</view>
+        <view :class="{ 'cc-dialog-content-content-padding': !$slots.content }" class="cc-dialog-content-content" v-if="content || $slots.content">
           <slot v-if="$slots.content" name="content"></slot>
           <view v-else>{{ content }}</view>
         </view>
@@ -31,23 +13,9 @@
           <view>
             <slot v-if="$slots.button" name="button"></slot>
             <template v-else>
-              <view
-                v-if="showCancelButton"
-                class="cc-dialog-content-button-cancel"
-                :style="{ color: cancelColor }"
-                @click="cancel"
-              >
-                {{ cancelText }}
-              </view>
-              <view
-                v-if="showConfirmButton"
-                class="cc-dialog-content-button-confirm"
-                :style="{ color: confirmColor }"
-                @click="confirm"
-              >
-                <view class="loading" v-if="loading">
-                  <cc-icon type="spinner-cycle" size="16" color="#c8c9cc"></cc-icon>
-                </view>
+              <view v-if="showCancelButton" class="cc-dialog-content-button-cancel" :style="{ color: cancelColor }" @click="cancel">{{ cancelText }}</view>
+              <view v-if="showConfirmButton" class="cc-dialog-content-button-confirm" :style="{ color: confirmColor }" @click="confirm">
+                <view class="loading" v-if="loading"><cc-icon type="spinner-cycle" size="16" color="#c8c9cc"></cc-icon></view>
                 <view v-else>{{ confirmText }}</view>
               </view>
             </template>
@@ -79,7 +47,7 @@ export default {
     // 弹框宽度
     width: {
       type: [Number, String],
-      default: 510
+      default: 640
     },
     // 是否展示确认按钮
     showConfirmButton: {
@@ -128,37 +96,38 @@ export default {
       loading: false,
       // 导航栏高度
       top: ''
-    };
+    }
   },
   methods: {
     // 确认事件
     confirm() {
       if (this.asyncClose) {
-        this.loading = true;
+        this.loading = true
       } else {
-        this.$emit('update:show', !this.show);
-        this.loading = false;
+        this.$emit('update:show', !this.show)
+         this.loading = false
       }
-      this.$emit('confirm');
+      this.$emit('confirm')
     },
     // 取消事件
     cancel() {
-      this.$emit('update:show', !this.show);
-      this.$emit('cancel');
+      this.$emit('update:show', !this.show)
+      this.$emit('cancel')
     },
     // 点击遮罩层
     clickMask() {
-      if (this.closeOnClickOverlay) this.$emit('update:show', !this.show);
+      if (this.closeOnClickOverlay) this.$emit('update:show', !this.show)
     },
     // 关闭
     close() {
-      this.$emit('update:show', !this.show);
+     
+      this.$emit('update:show', !this.show)
     }
   },
   mounted() {
     // #ifdef H5
-    let head = document.getElementsByTagName('uni-page-head')[0];
-    if (head) this.top = '88rpx';
+    let head = document.getElementsByTagName('uni-page-head')[0]
+    if (head) this.top = '88rpx'
     // #endif
   },
   onLoad() {},
@@ -167,26 +136,25 @@ export default {
   computed: {},
   watch: {
     show(val) {
-      if (val === false) this.loading = false;
+      if (val === false) this.loading = false
       if (val) {
-        this.display = 'block';
+        this.display = 'block'
       } else {
         setTimeout(() => {
-          this.display = 'none';
-        }, 80);
+          this.display = 'none'
+        }, 80)
       }
-    }
+    },
   }
-};
+}
 </script>
 
 <style scoped lang="scss">
 .cc-dialog {
   position: fixed;
   top: 34%;
-  left: 120rpx;
-  // left: 6%;
-  // right: 6%;
+  left: 6%;
+  right: 6%;
   overflow: hidden;
   font-size: 16px;
   background-color: #fff;
